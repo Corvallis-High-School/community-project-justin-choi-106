db9b8a62d348d43709132cdf444a277094fa76e8
justin-choi-106 <justin.choi@student.csd509j.net>
2025-12-04 17:35:13 +0000

    hee

--- CommunityProject.java
+++ CommunityProject.java
@@ -1,18 +1,80 @@
-public class CommunityProject {
+import java.util.ArrayList;
+
+
+
+public class  FrcTeam        // Add your class name here!
+{
     // 1. write 3 instance variables for class: private type variableName;
+    private  int teamNum = 0;
+private  ArrayList<String> membersName = new ArrayList<String>();
+    private  int hours = 0;
+//     private static String[] membersName = {
+//     null
+//     };
 
+    
     // 2. Add a constructor with 3 parameters to set all of the instance variables to the given parameters.
-
+    public FrcTeam (int teamNum, ArrayList<String> membersName, int hours) {
+        this.teamNum = teamNum;
+        this.membersName = membersName;
+        this.hours = hours;
+    }
     // 3. Write a print() method that uses System.out.println to print out all the instance variables.
-
+    public  void print() {
+      System.out.println( hours + " hours of pratice a week and team number is:" + teamNum);
+        for (String name : membersName) {
+      System.out.println(name);
+      }
+    }
     // 4. Create accessor (get) methods for each of the instance variables.
-
+    public  int getHours() {
+        return hours;
+    }
+     public  ArrayList<String> getMembers() {
+       return membersName;
+      }
+         
+    
+    
+     public  int getTeamNum() {
+        return teamNum;
+    }
+    
+   
     // 5. Create mutator (set) methods for each of the instance variables.
-
+      public  void setHours(int hoursAdded) {
+        hours = hours + hoursAdded;
+    }
+    
+      public  void setMembers(String membersAdded) {
+        membersName.add(membersAdded);
+          
+          
+    }
+   
+    
+   
+      public  void setTeamNum(int teamNumSet) {
+        teamNum = teamNumSet;
+      }
     // 6. Create a toString() method that returns all the information in the instance variables.
-
-    // 7. Write an additional method for your class that takes a parameter. For example, there could be a print method with arguments that indicate how you want to print out the information, e.g. print(format) could print the data according to an argument that is "plain" or "table" where the data is printed in a table drawn with dashes (-) and lines (|).
-
-    // 8. Write a main method that constructs at least 2 objects of your class using the constructor and then calls all of the methods that you created above to test them.
-
+      @Override
+    public String toString() {
+           return  
+               teamNum + hours +
+                     " " +  membersName;
+//                System.out.print(" " + membersName);
+//                }
+        }
+    // 7. Write an additional method for your class that takes a parameter.
+    // For example, there could be a print method with arguments that indicate how you want to print out
+    // the information, e.g. print(format) could print the data according to an argument that is "plain"
+    // or "table" where the data is printed in a table drawn with dashes and lines (|).
+ public void setTeamNum(int teamNumSet) {
+        teamNum = teamNumSet;
+      }
+    
+    // 8. Write a main method that constructs at least 2 objects of your class
+    // using the constructor and then calls all of the methods that you created above to test them.
+    
 }
--- Main.java
+++ Main.java
@@ -3,6 +3,17 @@ class Main {
   public static void main(String[] args) {
     // Construct 2 objects of your class using the constructor with different values
 
+        
+       // Construct 2 objects of your class using the constructor with different values
+      FrcTeam frcTeam1 = new FrcTeam(995, new ArrayList<String>(), 5);
+              FrcTeam frcTeam2 = new FrcTeam(749, new ArrayList<String>(), 5);
+        frcTeam1.print();
+        System.out.println(frcTeam1.toString());
+        frcTeam2.print();
+        
+       // call all of the objects methods to test them
+
+    
     // call all of the objects methods to test them
   }
 }
