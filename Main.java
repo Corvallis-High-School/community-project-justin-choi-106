6094722f20591f7caba61b8007f74d334eea789e
justin-choi-106 <justin.choi@student.csd509j.net>
2025-12-09 16:57:09 +0000

    six

--- Main.java
+++ Main.java
@@ -11,9 +11,30 @@ class Main {
       FrcTeam frcTeam1 = new FrcTeam(995, new ArrayList<String>(), 5);
               FrcTeam frcTeam2 = new FrcTeam(749, new ArrayList<String>(), 5);
         frcTeam1.print();
+        frcTeam1.setHours(2);
+        frcTeam1.setMembers("Issac");
+        frcTeam1.setTeamNum(0);
+        frcTeam1.getHours();
+
+                frcTeam1.getTeamNum();
+        frcTeam1.getMembers();
+
         System.out.println(frcTeam1.toString());
         frcTeam2.print();
         
+  frcTeam2.setHours(2);
+        frcTeam2.setMembers("Issac");
+        frcTeam2.setTeamNum(0);
+        frcTeam2.getHours();
+
+                frcTeam2.getTeamNum();
+        frcTeam2.getMembers();
+
+
+
+
+                System.out.println(frcTeam1.toString());
+
        // call all of the objects methods to test them
 
     
